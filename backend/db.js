a9b1e593e68fdd63a7c077247c59417f49ab4423
Rahul Kumar <mutcherlarahulkumar@gmail.com>
2024-04-14 11:27:37 +0530

    Final Paytm clone draft

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/paytm")
+mongoose.connect("mongodb+srv://rahul:hDzJYkAenZIbyQLR@cluster0.qkwlpub.mongodb.net/")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react"
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import axios from "axios"
 
 export const Dashboard = () => {
+
+    const [balance,setBalance] = useState("");
+
+    async function getdata(){
+        await axios.get("http://localhost:3000/api/v1/account/balance",{
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("token")
+            }
+        })
+        .then((response)=>{
+                console.log(typeof(response.data));
+                setBalance(response.data.balance)
+            })
+        
+    }
+    getdata();
+        
+
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance value={balance} />
             <Users />
         </div>
     </div>
